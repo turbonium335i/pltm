b35dd221ed5fa40e0660d6530822d7e49c1b682e
unknown <turbonium335i@gmail.com>
2022-06-02 19:56:37 +0900

    admin last login

--- cbtsystem/admin.py
+++ cbtsystem/admin.py
@@ -1,12 +1,20 @@
 from django.contrib import admin
 from .models import *
 
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
 class testRecordAdmin(admin.ModelAdmin):
     search_fields = ('studentUsername', 'studentName', 'testName', 'date_finished')
 
 class testSpecAdmin(admin.ModelAdmin):
     search_fields = ('name', 'code', 'notes')
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'last_login')
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
 
 # admin.site.register(testRecord)
 admin.site.register(testInProgress)
